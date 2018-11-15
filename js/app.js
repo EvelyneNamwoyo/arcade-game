45b91eb594873ee6be3daed9d528e5552d7cae4e
Evelyne Namwoyo <evelyne@laboremus.no>
2018-11-15 08:49:16 +0300

    Create Enemy class

--- js/app.js
+++ js/app.js
@@ -1,26 +1,69 @@
 // Enemies our player must avoid
-var Enemy = function() {
+// var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+//     this.sprite = 'images/enemy-bug.png';
+// };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+// Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+// };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+// Enemy.prototype.render = function() {
+//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+// };
+class Enemy{
+    constructor(x,y,speed){
+        this.sprite = 'images/enemy-bug.png';
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+       
+    }
+    update(dt){
+        if (this.x < 500){
+            this.x += this.speed*dt;
+        }
+        else{
+            this.x = 0;
+        }
+    }
 
+
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);  
+    }
+}
+class Player{
+    constructor(x,y){
+        this.sprite = 'images/char-boy.png';
+        this.x = 0;
+        this.y = 430;
+    }
+    update(dt){
+        // width = 500;
+        // while(){
+
+        // }
+
+    }
+
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);  
+    }
+
+    handleInput(){
+
+    }
+}
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
@@ -29,8 +72,9 @@ Enemy.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+ 
+const allEnemies = [new Enemy(0,60,100),new Enemy(0,145,150),new Enemy(0,230,200)];
+const player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
